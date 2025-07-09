d6f651d1fdf27eb13f862d00c918f28714f28295
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-10 00:01:58 +0400

    Async send email task created,Parthner admin created

--- bolbol/shops/admin/__init__.py
+++ bolbol/shops/admin/__init__.py
@@ -1,3 +1,4 @@
 from .shop_admin import *
 from .shop_registration_admin import *
 from .shop_activity_admin import *
+from .partner_admin import *
--- bolbol/shops/admin/partner_admin.py
+++ bolbol/shops/admin/partner_admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from shops.models import PartnerCompany
 
 
-@admin.register(PartnerCompany)
 class PartnerCompanyAdmin(admin.ModelAdmin):
     list_display = ("name", "url", "is_active")
     list_filter = ("is_active",)
     search_fields = ("name", "url")
+
+admin.site.register(PartnerCompany, PartnerCompanyAdmin)
