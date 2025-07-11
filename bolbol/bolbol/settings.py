b941d8ad3d6932d1da8c84099c14c7e1e98433c8
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-07-12 02:02:58 +0400

    updated .env

--- bolbol/bolbol/settings.py
+++ bolbol/bolbol/settings.py
@@ -329,7 +329,8 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazo
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
