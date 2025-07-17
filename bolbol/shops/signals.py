288078c48d0f656efcff4165205302d5728471ff
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-17 20:43:19 +0400

    Shop tasks and signals created.Now email send to user when he is try to send request shop regitser

--- bolbol/shops/signals.py
+++ bolbol/shops/signals.py
@@ -1,8 +1,14 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
+from django.db import transaction
 
-from shops.models import Shop
+from shops.models import Shop, ShopRegistrationRequest
 from shops.documents import ShopDocument
+from shops.tasks import (
+    send_shop_registration_request_create_email_task,
+    send_shop_registration_request_approved_email_task,
+    send_shop_registration_request_rejected_email_task,
+)
 
 
 # Elasticsearch signals
@@ -17,3 +23,32 @@ def delete_product_document(sender, instance, **kwargs):
         ShopDocument().get(id=instance.id).delete()
     except Exception:
         pass
+
+
+# Email signals
+@receiver(post_save, sender=ShopRegistrationRequest)
+def send_shop_registration_request(sender, instance, created, **kwargs):
+    if created:
+        def _send_email():
+            user_email = instance.shop_owner.email
+            send_shop_registration_request_create_email_task.delay(user_email)
+        transaction.on_commit(_send_email)
+
+
+@receiver(pre_save, sender=ShopRegistrationRequest)
+def check_status_change_on_shop_registration_request(sender, instance, **kwargs):
+    user_email = instance.shop_owner.email
+
+    if not instance.id:
+        return
+    try:
+        previous = ShopRegistrationRequest.objects.get(id=instance.id)
+    except ShopRegistrationRequest.DoesNotExist:
+        return
+
+    if previous.status != instance.status:
+        if instance.status == ShopRegistrationRequest.APPROVED:
+            send_shop_registration_request_approved_email_task.delay(user_email)
+
+        elif instance.status == ShopRegistrationRequest.REJECTED:
+            send_shop_registration_request_rejected_email_task.delay(user_email)
