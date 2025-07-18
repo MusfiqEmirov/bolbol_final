248f14066f2aa0dc7df3800022f5e6e4340de2f2
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-18 20:00:54 +0400

    fixed some bugs

--- bolbol/apis/views/users/user_views.py
+++ bolbol/apis/views/users/user_views.py
@@ -19,6 +19,7 @@ __all__ = (
     'ProductDetailByUserAPIView',
     'ProductPendingListByUserAPIView',
     'ProductApprovedListByUserAPIView',
+    'ProductsExpireAtListByUserAPIView',
 )
 
 User = get_user_model()
